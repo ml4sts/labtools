fddef0b16c2673dacd1bceb32909187ee39916fa
Sarah M Brown <brownsarahm@uri.edu>
2025-08-07 21:00:39 -0400

    file open

--- labtools/utils.py
+++ labtools/utils.py
@@ -6,7 +6,7 @@ def run(*args):
     return subprocess.check_call(['git'] + list(args))
 
 def git_branch(name):
-    run("checkout", "-b", br)
+    run("checkout", "-b", name)
 
 
 def load_template_file(template_file):
@@ -14,7 +14,7 @@ def load_template_file(template_file):
     load a template file from the package's template dir
     '''
     template_path = os.path.join(files(__package__), 'templates', template_file)
-    with template_path.open('r') as tmpt_f:
+    with open(template_path,'r') as tmpt_f:
         template = tmpt_f.read()
 
     return template
