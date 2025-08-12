bdb98afc067136da1eb47a57a5f4bb5a25ace5c2
Ritta Neg Mfa <ritta.negmfa@uri.edu>
2025-08-12 06:34:19 -0400

    Intergrating scraper into labtool

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='labtools',
       packages=['labtools'],
       zip_safe=False,
       include_package_data = True,
-      install_requires=['click'],
+      install_requires=['click','bs4',''],
       entry_points = {
         'console_scripts': ['lab=labtools.cli:lab',]}
      )
