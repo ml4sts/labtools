5ec9988621cddd9833ef05ee26bff9e28f073dbd
Sarah Brown <brownsarahm@uri.edu>
2025-08-25 17:33:49 -0400

    Update markdown_it to markdown-it-py in requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='labtools',
       packages=['labtools'],
       zip_safe=False,
       include_package_data = True,
-      install_requires=['click','markdown_it'],
+      install_requires=['click','markdown-it-py'],
       entry_points = {
         'console_scripts': ['lab=labtools.cli:lab',]}
      )
